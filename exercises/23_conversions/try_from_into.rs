6a5664b27f034b02ed1de2aab4a36999a299f1bc
BlackTea13 <robertblum2001@gmail.com>
2024-05-02 13:27:22 +0700

    conversions

--- exercises/23_conversions/try_from_into.rs
+++ exercises/23_conversions/try_from_into.rs
@@ -5,7 +5,7 @@
 // difference is that this should return a Result type instead of the target
 // type itself. You can read more about it at
 // https://doc.rust-lang.org/std/convert/trait.TryFrom.html
-//
+
 // Execute `rustlings hint try_from_into` or use the `hint` watch subcommand for
 // a hint.
 
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -37,10 +35,23 @@ enum IntoColorError {
 // time, but the slice implementation needs to check the slice length! Also note
 // that correct RGB color values must be integers in the 0..=255 range.
 
+fn parse_int(a: i16, b: i16, c: i16) -> Result<Color, IntoColorError> {
+    let a = u8::try_from(a).map_err(|_| IntoColorError::IntConversion)?;
+    let b = u8::try_from(b).map_err(|_| IntoColorError::IntConversion)?;
+    let c = u8::try_from(c).map_err(|_| IntoColorError::IntConversion)?;
+
+    return Ok(Color {
+        red: a,
+        green: b,
+        blue: c,
+    });
+}
+
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        return parse_int(tuple.0, tuple.1, tuple.2);
     }
 }
 
@@ -48,6 +59,7 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        return parse_int(arr[0], arr[1], arr[2]);
     }
 }
 
@@ -55,6 +67,11 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+
+        return parse_int(slice[0], slice[1], slice[2]);
     }
 }
 
